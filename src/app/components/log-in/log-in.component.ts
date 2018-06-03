6862db07a4ed43492b5cac58ab1ea7dfbafcc765
dbstroin <dbstroin@student.avans.nl>
2018-06-03 14:40:14 +0200

    Oops verkeerde branch...

---
+++ src/app/components/log-in/log-in.component.ts
@@ -0,0 +1,22 @@
+import { AuthorizationService } from "../../services/authorization.service";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'login-component',
+    templateUrl: './log-in.component.html',
+    styleUrls: ['./log-in.component.scss']
+})
+export class LoginComponent {
+
+    constructor(
+        private auth: AuthorizationService
+    ) {}
+
+    public login() {
+        this.auth.login();
+    }
+
+    public logout() {
+        this.auth.logout();
+    }
+}
