a83f54da56fac5b1405e860d857da6c1ddcff6a8
Rik van den Heuvel <rheuvel6@student.avans.nl>
2018-06-09 17:20:01 +0200

    Tournament generation v1.2 +Poule generation v1.0

--- src/app/components/competition-form/competition-form.component.ts
+++ src/app/components/competition-form/competition-form.component.ts
@@ -4,16 +4,18 @@ import { CompetitionService } from "../../services/competition.service";
 import { UserService } from "../../services/user.service";
 import { Competition } from "../../models/competition";
 import { User } from "../../models/user";
+import { DragulaModule } from 'ng2-dragula';
 
 @Component({
     selector: 'competition-form',
     templateUrl: './competition-form.component.html',
-    styleUrls: ['./competition-form.component.scss'],
+    styleUrls: ['./competition-form.component.scss', "../../../../node_modules/dragula/dist/dragula.css"],
 })
 export class CompetitionFormComponent implements OnInit {
 
     @Input() competition: Competition = new Competition();
     private competitionId: string;
+    private poules: any[];
 
     constructor(
         private competitionService: CompetitionService,
@@ -22,12 +24,36 @@ export class CompetitionFormComponent implements OnInit {
         private router: Router
     ) { }
 
+    public generatePoules() {
+        var poules = [];
+        var playersPerPoule = this.competition.participants.length / this.competition.numberOfPoules;
+        var participants = [];
+
+        for (var i = 0; i < this.competition.participants.length; i++) {
+            participants.push(this.competition.participants[i]);
+        }
+
+        for (var i = 0; i < this.competition.numberOfPoules; i++) {
+            var pouleParticipants = [];
+            var poule: any = {};
+            for (var j = 0; j < playersPerPoule; j++) {
+                pouleParticipants.push(participants[j] as User);
+            }
+            participants.splice(participants[0], playersPerPoule)
+            poule.participants = pouleParticipants;
+            poules.push(poule);
+        }
+        this.poules = poules;
+    }
+
     onSubmit(form: any): void {
         this.competition.rounds = [];
         this.competition.creator = null;
         this.competition.winner = null;
         this.competition.dateInMs = new Date(this.competition.date).getTime();
-        console.log(this.competition);
+        if (this.competition.type == "POULE") {
+            this.competition.poules = this.poules;
+        }
         if (this.competitionId) {
             this.competitionService.updateCompetition(this.competitionId, this.competition);
         } else {
--- src/app/services/competition.service.ts
+++ src/app/services/competition.service.ts
@@ -42,9 +42,9 @@ export class CompetitionService {
                         this.matchService.deleteMatch(match.id);
                     });
                 });
-                this.competitions.remove(competition.id);
             }
         });
+        this.competitions.remove(key);
     }
 
     public createCompetition(competition: Competition) {
@@ -52,7 +52,9 @@ export class CompetitionService {
             case "TOURNAMENT":
                 this.generateTournament(competition);
                 break;
-            case "POULE": break;
+            case "POULE":
+                this.generatePoule(competition);
+                break;
             case "KNOCKOUT": break;
         }
         var newCompetition = competition;
@@ -66,18 +68,80 @@ export class CompetitionService {
         this.competitions.update(id, updatedCompetition);
     }
 
-    public generateTournament(competition: Competition) {
+    public calculateUniqueMatches(participants: User[]) {
+        var participantCombos = [];
+
+        for (var i = 0; i < participants.length; i++) {
+            for (var j = 0; j < participants.length; j++) {
+                var user1 = participants[i];
+                var user2 = participants[j];
+                var shouldAdd = true;
+                if (participantCombos.length != 0) {
+                    for (var k = 0; k < participantCombos.length; k++) {
+                        var participantCombo = participantCombos[k];
+                        if (this.arraysEqual(participantCombo.users, [user1.id, user2.id])) {
+                            shouldAdd = false;
+                            continue;
+                        }
+                    }
+                }
+                if (user1.id == user2.id) {
+                    shouldAdd = false;
+                }
+                if (shouldAdd) {
+                    participantCombos.push({ users: [user1.id, user2.id]/*, occurences: 0*/ })
+                }
+            }
+        }
+        return participantCombos;
+    }
+
+    public generatePoule(competition: Competition) {
         var rounds: Round[] = [];
         var roundsReferences: Round[] = [];
-        for (var i = 0; i < (competition.participants.length / 2); i++) {
+
+        for (var i = 0; i < competition.numberOfPoules; i++) {
+            var pouleParticipants: any[] = competition.poules[i].participants;
+            var participantCombos = this.calculateUniqueMatches(pouleParticipants);
+            var participantCombosCopy: any[] = [];
+
+            for (var j = 0; j < participantCombos.length; j++)
+                participantCombosCopy.push(participantCombos[j]);
+
             var round = new Round();
-            round.number = i;
-            round.matches = this.generateMatches(competition.participants, rounds, competition, i);
+            round.number = i + 1;
+            round.matches = this.generateMatches(participantCombos, participantCombosCopy, rounds, competition, i);
             rounds.push(round);
         }
 
+        //TODO: EEN SPELER MAG NIET 2X OM DEZELFDE TIJD SPELEN!!
+
+        this.createMatches(rounds, roundsReferences);
+        competition.rounds = roundsReferences;
+    }
+
+    public generateTournament(competition: Competition) {
+        var participantCombos = this.calculateUniqueMatches(competition.participants);
+        var rounds: Round[] = [];
+        var roundsReferences: Round[] = [];
+        var participantCombosCopy: any[] = [];
+        for (var i = 0; i < participantCombos.length; i++)
+            participantCombosCopy.push(participantCombos[i]);
+
+        for (var i = 0; i < competition.numberOfRounds; i++) {
+            var round = new Round();
+            round.number = i + 1;
+            round.matches = this.generateMatches(participantCombos, participantCombosCopy, rounds, competition, i);
+            rounds.push(round);
+        }
+        this.createMatches(rounds, roundsReferences);
+        competition.rounds = roundsReferences;
+    }
+
+    public createMatches(rounds: Round[], roundsReferences: Round[]) {
         rounds.forEach(round => {
             var roundReference = new Round();
+            roundReference.number = round.number;
             roundReference.matches = [];
             round.matches.forEach(match => {
                 var ref = this.matchService.createMatch(match);
@@ -85,75 +149,90 @@ export class CompetitionService {
             });
             roundsReferences.push(roundReference);
         });
-        competition.rounds = roundsReferences;
+    }
+
+    public generateMatches(participantCombosReference: any[], participantCombosPool: any[], rounds: Round[], competition: Competition, multiplier: number): Match[] {
+        var matches = [];
+        var startingTime = new Date(competition.date);
+        startingTime.setTime(startingTime.getTime() + (competition.minutesPerMatch * multiplier) * 60 * 1000);
+        var startingTimeInMs = startingTime.getTime();
+
+        for (var i = 0; i < participantCombosReference.length; i++) {
+            var match = new Match();
+            match.status = "OPEN";
+            match.creator = competition.creator;
+            match.startingTimeInMs = startingTimeInMs;
+            match.participants = [];
+
+            for (var j = 0; j < participantCombosReference[i].users.length; j++) {
+                var user1Id = participantCombosReference[i].users[0];
+                var user2Id = participantCombosReference[i].users[1];
+
+                if (matches.length < participantCombosReference.length / 2 || competition.type != "TOURNAMENT") {
+                    if (!this.havePlayedInThisRound(user1Id, user2Id, matches) || competition.type != "TOURNAMENT") {
+                        if (!this.havePlayedAgainstEachOther({ id: user1Id } as User, { id: user2Id } as User, rounds)) {
+                            if (match.participants.length < 2) {
+                                match.participants.push({ id: user1Id } as User);
+                                match.participants.push({ id: user2Id } as User);
+                                participantCombosPool.splice(participantCombosPool[i], 1)
+                                matches.push(match);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return matches;
     }
 
     public havePlayedAgainstEachOther(user1: User, user2: User, rounds: Round[]): boolean {
-        rounds.forEach(round => {
-            round.matches.forEach(match => {
-                if (match.participants.indexOf(user1) != -1 && match.participants.indexOf(user2)) {
-                    return true;
+        for (var i = 0; i < rounds.length; i++) {
+            for (var j = 0; j < rounds[i].matches.length; j++) {
+                var match = rounds[i].matches[j];
+                if (match && match.participants) {
+                    var users = match.participants;
+                    if (JSON.stringify(users) == JSON.stringify([{ id: user1.id }, { id: user2.id }])) {
+                        return true;
+                    }
+                    if (JSON.stringify(users) == JSON.stringify([{ id: user2.id }, { id: user1.id }])) {
+                        return true;
+                    }
                 }
-            });
-        });
-        return false;
-    }
-
-    public havePlayedInThisRound(user1: User, user2: User, participants: User[]): boolean {
-        if (participants.indexOf(user1) != -1 || participants.indexOf(user2) != -1) {
-            return true;
-        }
-        return false;
-    }
-
-    public generateUniqueMatch(competitionParticipants: User[], rounds: Round[], competition: Competition, multiplier: number): Match {
-        var user1Index = 0;
-        var user2Index = 0;
-
-        var startingTime = new Date(competition.date);
-        startingTime.setTime(startingTime.getTime() + (competition.minutesPerMatch * multiplier) * 60 * 1000);
-        var startingTimeInMs = startingTime.getTime()
-
-        var match = new Match();
-        match.status = "OPEN";
-        match.creator = competition.creator;
-        match.startingTimeInMs = startingTimeInMs;
-        match.participants = [];
-        do {
-            var user1Index = Math.floor(Math.random() * (competitionParticipants.length));
-            var user2Index = Math.floor(Math.random() * (competitionParticipants.length));
-        } while (user1Index == user2Index);
-        var user1 = competitionParticipants[user1Index];
-        var user2 = competitionParticipants[user2Index];
-
-        if (this.havePlayedInThisRound(user1, user2, match.participants)) {
-            this.generateUniqueMatch(competitionParticipants, rounds, competition, multiplier);
-        } else {
-            match.participants.push(user1);
-            match.participants.push(user2);
-
-            if (this.havePlayedAgainstEachOther(user1, user2, rounds)) {
-                this.generateUniqueMatch(competitionParticipants, rounds, competition, multiplier);
-            } else {
-                competitionParticipants.splice(user1Index, 1);
-                competitionParticipants.splice(user2Index, 1);
-                return match;
             }
         }
+        return false;
     }
 
-    public generateMatches(participants: User[], rounds: Round[], competition, multiplier: number): Match[] {
-        var competitionParticipants = [];
-        var matches = [];
-
-        for (var i = 0, len = participants.length; i < len; ++i) {
-            competitionParticipants[i] = participants[i];
+    public havePlayedInThisRound(user1Id: string, user2Id: string, matches: Match[]) {
+        if (matches.length > 0) {
+            for (var i = 0; i < matches.length; i++) {
+                for (var j = 0; j < matches[i].participants.length; j++) {
+                    var users = matches[i].participants;
+                    if (JSON.stringify(users).indexOf(JSON.stringify({ id: user1Id })) != -1) {
+                        return true;
+                    }
+                    if (JSON.stringify(users).indexOf(JSON.stringify({ id: user2Id })) != -1) {
+                        return true;
+                    }
+                }
+            }
         }
+        return false;
+    }
 
-        for (var i = 0; i < participants.length / 2; i++) {
-            matches.push(this.generateUniqueMatch(competitionParticipants, rounds, competition, multiplier));
+    public arraysEqual(_arr1: any[], _arr2: any[]): boolean {
+
+        if (!Array.isArray(_arr1) || !Array.isArray(_arr2) || _arr1.length !== _arr2.length)
+            return false;
+
+        var arr1 = _arr1.concat().sort();
+        var arr2 = _arr2.concat().sort();
+        for (var i = 0; i < arr1.length; i++) {
+
+            if (arr1[i] !== arr2[i])
+                return false;
+
         }
-
-        return matches;
+        return true;
     }
 }
