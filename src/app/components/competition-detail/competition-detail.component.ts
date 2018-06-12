553cbd548b7a5d53fb71f944730db4f846e03f8a
dbstroin <dbstroin@student.avans.nl>
2018-06-12 15:44:15 +0200

    User can now add himself to a competition
    BUG: When adding/removing yourself from participants it will get all
    users in the participation table.

--- src/app/components/competition-detail/competition-detail.component.ts
+++ src/app/components/competition-detail/competition-detail.component.ts
@@ -4,6 +4,8 @@ import { CompetitionService } from "../../services/competition.service";
 import { Competition } from "../../models/competition";
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/User';
+import { AuthorizationService } from '../../services/authorization.service';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
     selector: 'competition-detail',
@@ -15,13 +17,18 @@ export class CompetitionDetailComponent {
     @Input() competition: Competition = new Competition();
     public creator: User = new User();
     public participants: User[] = [];
+    public canParticipate = true;
+    public alreadyParticipating = false;
+
     private competitionId: string;
 
     constructor(
         private competitionService: CompetitionService,
         private userService: UserService,
         private route: ActivatedRoute,
-        private router: Router
+        private router: Router,
+        private auth: AuthorizationService,
+        private notificationService: NotificationService
     ) { }
 
     ngOnInit() {
@@ -33,10 +40,15 @@ export class CompetitionDetailComponent {
                     this.userService.getUser(this.competition.creator).snapshotChanges().subscribe(user => {
                         this.creator = {id: user.key, ...user.payload.val()};
                     });
+
                     this.competition.participants.forEach(participant => {
                         this.userService.getUser(participant.id).snapshotChanges().subscribe(user => {
                             let u = {id: user.key, ...user.payload.val()};
                             this.participants.push(u);
+                            if (u.id === this.auth.user.uid && this.canParticipate) {
+                                this.alreadyParticipating = true;
+                                this.canParticipate = false;
+                            }
                         });
                     });
                 } else {
@@ -44,5 +56,40 @@ export class CompetitionDetailComponent {
                 }
             });
         }
+    }
+    
+    public joinCompetition() {
+        let canJoin = true;
+        this.participants.forEach(participant => {
+            if (participant.id === this.auth.user.uid) {
+                canJoin = false;
+            }
+        });
+
+        if (canJoin && this.competition.participants.length < this.competition.maxAmountOfParticipants && this.competition.date <= new Date()) {
+            this.auth.getCurrentUser().subscribe(user => {
+                let u = user as User;
+                this.competition.participants.push(u);
+                this.competitionService.updateCompetition(this.competition.id, this.competition)
+                this.canParticipate = false;
+                this.alreadyParticipating = true;
+                this.notificationService.sendSuccess('Joined the competition');
+            });
+        } else {
+            this.canParticipate = false;
+            this.notificationService.sendError('You cannot join this competition');
+        }
+    }
+
+    public leaveCompetition() {
+        this.auth.getCurrentUser().subscribe(user => {
+            let u = user as User;
+            let index = this.competition.participants.indexOf(u);
+            this.competition.participants.splice(index, 1);
+            this.competitionService.updateCompetition(this.competition.id, this.competition)
+            this.canParticipate = true;
+            this.alreadyParticipating = false;
+            this.notificationService.sendSuccess('Left the competition');
+        });
     }
 }
