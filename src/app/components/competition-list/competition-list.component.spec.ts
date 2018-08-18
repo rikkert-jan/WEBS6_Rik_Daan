2c4128be949fedb2a591d954787709ccda783929
dbstroin <dbstroin@student.avans.nl>
2018-08-18 16:53:10 +0200

    Added empty test

---
+++ src/app/components/competition-list/competition-list.component.spec.ts
@@ -0,0 +1,7 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { CompetitionListComponent } from './competition-list.component';
+
+describe('competiton-list', () => {
+
+});
